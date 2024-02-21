b3ce8f7d844e5c111840b1fe8cdb0683a37b0b6d
yuandmi <yumi930330@gmail.com>
2024-02-21 00:38:06 +0000

    updated

---
+++ NECO_groupwork/matlab/fitcnet.m
@@ -0,0 +1,28 @@
+%% Training MLP Model %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% clear command, workspace, and figures
+clc;
+clear;
+close all;
+
+%% For reproducibility
+rng("default")
+
+%% Load data
+X_train = readtable(['X_train.csv']);
+y_train = readtable(['y_train.csv']);
+X_val = readtable(['X_val.csv']);
+y_val = readtable(['y_val.csv']);
+X_test = readtable(['X_test.csv']);
+y_test = readtable(['y_test.csv']);
+
+%% Table to double
+X_train = table2array(X_train);
+y_train = table2array(y_train);
+X_val = table2array(X_val);
+y_val = table2array(y_val);
+X_test = table2array(X_test);
+y_test = table2array(y_test);
+
+%% Build a model
+Mdl = fitcnet(X_train,y_train)
